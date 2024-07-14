eab956e1a5d9b1f13d92834e484d14306b65142d
nlaky1 <49218507+nlaky1@users.noreply.github.com>
2024-07-15 00:13:37 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -15,6 +15,20 @@ const Properties = () => {
 
     return (
         <div className="container mx-auto p-4">
+            <div className="p-4 mx-auto relative z-10 w-full pt-10 md:pt-20 px-2">
+        <div className="text-4xl pb-5 md:text-7xl px-6 text-center  bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to bg-neutral-400 bg-opacity-50">
+          We are ABC Organisation, and <br />We provide you latest upcoming auctions
+        </div>
+        <p className="mt-4 text-lg font-normal  text-neutral-300 max-w-lg text-center mx-auto px-4">
+         Get the latest auction details 
+        </p>
+
+        <Link
+          href={"/book"}
+          className="cursor-pointer flex items-center justify-center border rounded-full w-48 p-2  mx-auto my-6 text-white "
+        >
+          Want to connect with us
+        </Link>
             <h1 className="text-2xl font-bold mb-4 text-black">ICICI Bank Properties for Sale</h1>
             <div className="overflow-x-auto">
                 <table className="min-w-full bg-white border border-gray-200">
